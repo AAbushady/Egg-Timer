d1012e2f2712e9d0e26be6581b54da822b3ae7a0
Alexander Abushady <aabushady214@gmail.com>
2019-08-03 16:00:59 -0400

    Initial commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Alexander Abushady on 8/3/19.
+//  Copyright © 2019 Alexander Abushady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
